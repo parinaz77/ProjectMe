e55abd7d3f26657638c02661e5bebc47a2922d56
parinaz77 <parinaz77@gmail.com>
2017-12-07 17:45:15 -0800

    add keys file to store google oauth keys

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const app = express();
 
+passport.use(new GoogleStrategy());
+
 app.get('/', (req, res) => {
 	res.send({ hi: 'there' });
 });
 
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 app.listen(PORT);
